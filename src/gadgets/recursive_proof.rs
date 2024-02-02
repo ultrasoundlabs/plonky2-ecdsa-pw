cb32e4524da87a79a382efb2c1c5fe004fa22eea
xiyu1984 <shanellyy@126.com>
2024-02-02 19:11:40 +0800

    less limitaion `recursive_proof_2`

--- src/gadgets/recursive_proof.rs
+++ src/gadgets/recursive_proof.rs
@@ -79,3 +79,65 @@ where
 
     Ok((proof, data.verifier_only, data.common))
 }
+
+pub fn recursive_proof_2<
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>,
+    InnerC: GenericConfig<D, F = F>,
+    const D: usize,
+>(
+    inner_proofs: &Vec<ProofTuple<F, InnerC, D>>,
+    config: &CircuitConfig,
+    min_degree_bits: Option<usize>,
+) -> Result<ProofTuple<F, C, D>>
+where
+    InnerC::Hasher: AlgebraicHasher<F>
+{
+    let mut builder = CircuitBuilder::<F, D>::new(config.clone());
+    let mut pw = PartialWitness::new();
+
+    for one_proof_ref in inner_proofs {
+        let (inner_proof, inner_vd, inner_cd) = one_proof_ref;
+        let pt = builder.add_virtual_proof_with_pis(inner_cd);
+        pw.set_proof_with_pis_target(&pt, inner_proof);
+        builder.register_public_inputs(&*pt.public_inputs);
+
+        let inner_data = VerifierCircuitTarget {
+            constants_sigmas_cap: builder.add_virtual_cap(inner_cd.config.fri_config.cap_height),
+            circuit_digest: builder.add_virtual_hash(),
+        };
+        pw.set_cap_target(
+            &inner_data.constants_sigmas_cap,
+            &inner_vd.constants_sigmas_cap,
+        );
+        pw.set_hash_target(inner_data.circuit_digest, inner_vd.circuit_digest);
+
+        builder.verify_proof::<InnerC>(&pt, &inner_data, inner_cd);
+    }
+
+   
+    builder.print_gate_counts(0);
+
+    if let Some(min_degree_bits) = min_degree_bits {
+        // We don't want to pad all the way up to 2^min_degree_bits, as the builder will
+        // add a few special gates afterward. So just pad to 2^(min_degree_bits
+        // - 1) + 1. Then the builder will pad to the next power of two,
+        // 2^min_degree_bits.
+        let min_gates = (1 << (min_degree_bits - 1)) + 1;
+        for _ in builder.num_gates()..min_gates {
+            builder.add_gate(NoopGate, vec![]);
+        }
+    }
+
+    let data = builder.build::<C>();
+
+    let mut timing = TimingTree::new("recursive:2 prove", Level::Info);
+    let proof = prove(&data.prover_only, &data.common, pw, &mut timing)?;
+    timing.print();
+
+    let timing = TimingTree::new("recursive:2 verify", Level::Info);
+    data.verify(proof.clone())?;
+    timing.print();
+
+    Ok((proof, data.verifier_only, data.common))
+}
